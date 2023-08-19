ab2d22d99c1744e8953b3577aea2a56e4092487b
Andrey <andrey.burtasov@yandex.ru>
2023-08-19 21:12:16 +0300

    removed app package

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,33 @@
 package main
 
-import "github.com/aburtasov/fibonaccisrv/pkg/app"
+import (
+	"log"
+
+	"github.com/aburtasov/fibonaccisrv/pkg/config"
+	"github.com/aburtasov/fibonaccisrv/pkg/handler"
+	"github.com/aburtasov/fibonaccisrv/pkg/storage"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 
-	app.Run()
+	cfg, err := config.NewConfig()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	storage := storage.NewRedisStorage(cfg.DBAddr)
+
+	handler := handler.NewHandler(storage)
+
+	router := gin.Default()
+
+	router.GET("/fibonacci/:x,y", handler.GetFibonacci)
+	router.POST("/fibonacci/:len", handler.CreateFibonacci)
+
+	if err := router.Run(cfg.HTTPAddr); err != nil {
+
+		log.Fatal(err)
+	}
 
 }
