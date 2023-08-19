6a6a641c04458deed40ba9cedc7ce3230933541d
Andrey <andrey.burtasov@yandex.ru>
2023-08-19 20:58:12 +0300

    removed logger

--- pkg/app/app.go
+++ pkg/app/app.go
@@ -1,24 +1,25 @@
 package app
 
 import (
+	"log"
+
 	"github.com/aburtasov/fibonaccisrv/pkg/config"
 	"github.com/aburtasov/fibonaccisrv/pkg/handler"
-	"github.com/aburtasov/fibonaccisrv/pkg/logger"
 	"github.com/aburtasov/fibonaccisrv/pkg/storage"
 	"github.com/gin-gonic/gin"
 )
 
 func Run() {
 
-	logger := logger.NewLogger()
-
 	cfg, err := config.NewConfig()
 	if err != nil {
-		logger.Fatal(err)
+		log.Fatal(err)
 	}
 
 	storage := storage.NewRedisStorage(cfg.DBAddr)
+
 	handler := handler.NewHandler(storage)
+
 	router := gin.Default()
 
 	router.GET("/fibonacci/:x,y", handler.GetFibonacci)
@@ -26,7 +27,7 @@ func Run() {
 
 	if err := router.Run(cfg.HTTPAddr); err != nil {
 
-		logger.Fatal(err)
+		log.Fatal(err)
 	}
 
 }
